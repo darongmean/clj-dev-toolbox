0ce53857c97c768edb762d7eb37ad22d45ddc15d
Darong Mean <contact.darong@gmail.com>
2024-08-29 08:05:24 +0200

    format

--- test/clj_dev_toolbox/clj_dev_toolbox_test.clj
+++
@@ -1,7 +0,0 @@
-(ns clj-dev-toolbox.clj-dev-toolbox-test
-  (:require [clojure.test :refer :all]
-            [clj-dev-toolbox.clj-dev-toolbox :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
